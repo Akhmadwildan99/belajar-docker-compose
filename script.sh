2273ebf0551ae061653c667252bf9f4474fb5dae
wildan <wildanakhmad3584@gmail.com>
2024-04-17 21:39:39 +0700

    init

---
+++ script.sh
@@ -0,0 +1,12 @@
+
+# create container
+docker compose create
+
+# start container
+docker compse start
+
+# melihat container per config file
+docker compose ps
+
+# menghentikan container per config file
+docker compose stop
